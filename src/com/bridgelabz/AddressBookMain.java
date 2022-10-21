3076bf8a45457ab670eb61a1095ca046cc36e6b6
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-21 19:42:20 +0530

    Wel-Come to Address book systems

---
+++ src/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class AddressBookMain
+{
+    public static void main(String[] args)
+    {
+        System.out.println("-:*@*  Wel-Come to Address book systems  *@*:-");
+    }
+}
